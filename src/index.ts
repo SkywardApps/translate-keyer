bf7507a4eefba4d937bef2e90fb057134a44919d
Nicholas Elliott <nelliott@skywardapps.com>
2023-06-27 14:37:29 -0400

    Re-export handler

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { initTranslateKeyer } from './Initialize';
 export { useTranslationWithDescription, TranslationProxyFunction, translate, useTranslated } from './useTranslationWithDescription';
 export { T } from './T';
+export { missingKeyHandler } from './Reporter';
